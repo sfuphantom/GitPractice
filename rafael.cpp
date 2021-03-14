fa1b9c0ebc3cd7dc4e87e169de9f4442739ea75f
rafguevara14 <rafaelg@sfu.ca>
2021-03-13 21:10:02 -0800

    candy

---
+++ rafael.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+int main(){
+
+	std::cout << "I like cars.";
+
+	bool ilovecandy = true;
+
+	std::cout << "Do I love candy?" << boolalpha << ilovecandy;
+
+	std::cout << "Do I not love candy?" << boolalpha << !ilovecandy;
+
+	std::cout << "Red Twizzlers are amazing but like so are hershey's";
+
+}
